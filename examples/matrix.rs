607861087285b24ef5de1a77bc1b8b731e091a0a
Luboy23 <2387443867@qq.com>
2024-09-19 03:29:56 +0800

    features: Metrics with HashMap and Mutex

--- examples/matrix.rs
+++ examples/matrix.rs
@@ -7,4 +7,4 @@ fn main() -> Result<()>{
     let b = Matrix::new([1, 2, 3, 4, 5, 6], 3, 2);
     println!("a * b:{}", a * b);
     Ok(())
-}
+} 
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 mod matrix;
 mod vecotr;
+mod metrics;
 
 pub use matrix::{multiply, Matrix};
 pub use vecotr::{Vector, dot_product};
+pub use metrics::Metrics;
